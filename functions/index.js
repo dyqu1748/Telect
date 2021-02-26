eaa32719e90f340d998405876e347fd9378d10da
Arthur May <arthur@raehraeh.com>
2021-02-26 16:43:31 -0700

    added availableLocations function which queries for Telect locations by function availablelocations/{state}/{city}

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,23 @@
 const functions = require("firebase-functions");
+const firebase = require("firebase-admin");
 
 // Set the configuration for your app
   // TODO: Replace with your project's config object
  var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+    apiKey: "AIzaSyC_RpCqU1yIDiyhEBadqOL-ph09x-7wzJg",
+    authDomain: "telect-6026a.firebaseapp.com",
+    databaseURL: "https://telect-6026a-default-rtdb.firebaseio.com",
+    projectId: "telect-6026a",
+    storageBucket: "telect-6026a.appspot.com",
+    messagingSenderId: "193710214424",
+    appId: "1:193710214424:web:d0269dce35f3a81b53226d",
+    measurementId: "G-GBP3LB0T8V"
+  };
+
  firebase.initializeApp(firebaseConfig)
 
 // Get a reference to the database service
-  var database = firebase.database();
+var database = firebase.database();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
@@ -22,8 +25,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.availableLocations = functions.https.onRequest((request, response) => {
-  functions.logger.info("Begin: activeLocations", {structuredData: true});
-
-  response.send("Hello from Firebase!");
-  functions.logger.info("End: activeLocations", {structuredData: true});
+    const params = request.url.split("/");
+    response.setHeader('Content-Type', 'application/json')
+    return firebase.database().ref('locations').once('value', (snapshot) => {
+        snapshot.forEach(function(childSnapshot) {
+        var location = childSnapshot.val();
+        if (location.state === params[1] && location.city === params[2]){
+            response.send({"success": true});
+        }
+        });
+        response.send({"success": false});
+     });
 });
