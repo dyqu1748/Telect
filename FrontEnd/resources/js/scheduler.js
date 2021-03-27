f39ad2e41656f15645c2eafe29a35bb760445b5d
fallynlogan <falo7122@colorado.edu>
2021-03-27 16:06:54 -0600

    make schedule JSON object

--- FrontEnd/resources/js/scheduler.js
+++ FrontEnd/resources/js/scheduler.js
@@ -5,5 +5,32 @@ function addItem(id) {
     } else if (document.getElementById(id).className == "scheduler_item_selected") {
         document.getElementById(id).className = "scheduler_item";
     }
+}
 
+function getScheduleDays() {
+    var divs = document.getElementsByTagName("div");
+    var days = []; 
+    var schedule = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [], 
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [], 
+        "Sunday": []
+    };
+    for(var i = 0; i < divs.length; i++) {
+        if (divs[i].className == "scheduler_item_selected") {
+            days.push(divs[i].id);
+        }   
+     }
+
+     for(var i = 0; i < days.length; i++) {
+         day_time = days[i].split("_");
+         day = day_time[0];
+         time = day_time[1];
+        schedule[day].push(time); 
+     }
+     var scheduleJSON = JSON.stringify(schedule);
+     return scheduleJSON;
 }
